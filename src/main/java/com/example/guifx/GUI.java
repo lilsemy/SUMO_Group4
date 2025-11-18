720aefe500cee5d2bd6eb5d6170398cc902774f9
lilsemy <werbinich@mailbox.org>
2025-11-18 18:48:37 +0100

    Enabled Insertion of multiple Cars over the GUI

--- src/main/java/com/example/guifx/GUI.java
+++ src/main/java/com/example/guifx/GUI.java
@@ -6,16 +6,18 @@ import javafx.fxml.FXML;
 import static com.example.guifx.Connection.conn;
 
 public class GUI {
+
+    private int id = 1;
+
     @FXML
     public void InsertCar(ActionEvent e) {
         System.out.println("Button clicked!");
-
-        int id = 1;
         String idf = "id" + id;
         byte lane1 = 0;
         try {
             conn.do_job_set(Vehicle.add(idf, "car", "route1", 0, 0.0, 1.0, lane1)); //Fügt Lastwagen hinzu. ACHTUNG: VehicleType "ev"/"tr" muss in dem .rou.xml File definiert werden.
             conn.do_job_set(Gui.trackVehicle("View #0", idf));
+            id += 1;
         } catch (Exception ex) {
             ex.printStackTrace();
         }
