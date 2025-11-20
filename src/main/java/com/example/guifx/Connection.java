b3499a382cf8bb74469f434a514addf43ce192ad
LarsMKeppler <110167150+LarsMKeppler@users.noreply.github.com>
2025-11-20 07:58:01 +0100

    Update Connection.java
    for-Schleife für Timesteps -> do-While

--- src/main/java/com/example/guifx/Connection.java
+++ src/main/java/com/example/guifx/Connection.java
@@ -25,6 +25,8 @@ public class Connection {
                 conn.runServer();
                 conn.setOrder(1);
 
+                // do {conn.do_timestep(); } while ((int) conn.do_job_get(Vehicle.getIDCount())) > 0);
+                
                 for (int i = 0; i < 200000; i++) {
                     conn.do_timestep();
                     double timeSeconds = (double) conn.do_job_get(Simulation.getTime());
