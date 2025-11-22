d8af0a05ab958dba2bef34453d370bb8deb8f2b4
LarsMKeppler <110167150+LarsMKeppler@users.noreply.github.com>
2025-11-22 13:25:00 +0100

    Update Launcher.java

--- src/main/java/com/example/guifx/Launcher.java
+++ src/main/java/com/example/guifx/Launcher.java
@@ -2,7 +2,14 @@ package com.example.guifx;
 
 import javafx.application.Application;
 
+    /**
+    *Launcher is a
+    */
+
 public class Launcher {
+    /**
+    *Starts the application
+    */
     public static void main(String[] args) {
         Application.launch(Main.class, args);
     }
