a5f3d2042aa7af12893188cb9a63e018eabe1c5f
LarsMKeppler <110167150+LarsMKeppler@users.noreply.github.com>
2025-11-22 13:22:41 +0100

    Update Connection.java
    JavaDoc

--- src/main/java/com/example/guifx/Connection.java
+++ src/main/java/com/example/guifx/Connection.java
@@ -6,11 +6,21 @@ import it.polito.appeal.traci.SumoTraciConnection;
 
 //TODO: for-each Schleife?
 
+/**
+* Connection is a
+*/
+
 public class Connection {
     private TraciConnect simulation;
     private VehiclesMangagement vehiclesManager;
 
     private GUI controller1;
+
+    /**
+    *
+    *@throws
+    */
+    
     public Connection() throws Exception {
         simulation =new TraciConnect();
         simulation.connect();
@@ -18,10 +28,15 @@ public class Connection {
         makeConnection();
     }
     //public static SumoTraciConnection conn;
-
+    
     public VehiclesMangagement getVehiclesManager(){
         return vehiclesManager;
     }
+
+    /**
+    * Establishes connection
+    */
+    
     public  void makeConnection()  {
 
 
@@ -33,6 +48,11 @@ public class Connection {
         double step_length = 0.1;*/
 
         new Thread(() -> {
+            
+            /**
+            *
+            *@throws
+            */
             try {
 
 
