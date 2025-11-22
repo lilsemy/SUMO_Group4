dcb4b232fad506c292f1fc8f43920c74f0f45aaa
LarsMKeppler <110167150+LarsMKeppler@users.noreply.github.com>
2025-11-22 13:30:14 +0100

    Update TraciConnect.java
    JavaDoc

--- src/main/java/com/example/guifx/TraciConnect.java
+++ src/main/java/com/example/guifx/TraciConnect.java
@@ -4,12 +4,22 @@ package com.example.guifx;
 
 import it.polito.appeal.traci.SumoTraciConnection;
 
+    /**
+    *TraciConnect is a
+    */
+
 public class TraciConnect {
 
 
     private SumoTraciConnection conn;
+    
     public TraciConnect(){}
     //new code
+    /**
+    *Establishes new connection
+    *@throws
+    */
+    
     public void connect()throws Exception{
         conn = new SumoTraciConnection(MapSumoConfig.sumo_bin, MapSumoConfig.config_file);
         conn.addOption("start","true");
@@ -20,16 +30,28 @@ public class TraciConnect {
 
     }
 
+    /**
+    *Does a timestep in the simulation
+    *@throws
+    */
     public void doStep() throws Exception{
         conn.do_timestep();
     }
 
+    
+    /**
+    *Closes the connection
+    *@throws
+    */
     public void close()throws Exception{
         if(conn != null){
             conn.close();
         }
     }
 
+    /**
+    *
+    */
     public SumoTraciConnection getConn() {
         return conn;
     }
