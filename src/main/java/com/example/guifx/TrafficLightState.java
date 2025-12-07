20f5129f147e70e941d8a632bb5595f13dcda571
LarsMKeppler <110167150+LarsMKeppler@users.noreply.github.com>
2025-12-07 19:37:17 +0100

    Update TrafficLightState.java

--- src/main/java/com/example/guifx/TrafficLightState.java
+++ src/main/java/com/example/guifx/TrafficLightState.java
@@ -4,14 +4,14 @@ package com.example.guifx;
  * TrafficLightState is a model class for Traffic Lights
  */
 
-public class TrafficLightModel {
+public class TrafficLightState {
     private String id;
     private int duration;
     private char state;
     private int minDur;
     private int maxDur;
 
-    public TrafficLightModel(){}
+    public TrafficLightState(){}
 
     public String getId(){return id;}
     public char getState(String id){return state;}
