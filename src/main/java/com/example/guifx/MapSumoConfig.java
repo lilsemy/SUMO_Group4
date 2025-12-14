773e9e53fd5762fb5a4709c2d460a8d4904877b4
ancaranovik <10423074@student.vgu.edu.vn>
2025-12-14 01:50:55 +0100

    adding .jar for fromating code and formatted files

--- src/main/java/com/example/guifx/MapSumoConfig.java
+++ src/main/java/com/example/guifx/MapSumoConfig.java
@@ -1,20 +1,15 @@
 package com.example.guifx;
 
-    /**
-    *MapSumoConfig is holding the Configuration Files for SUMO Simulation
-    */
-
+/** MapSumoConfig is holding the Configuration Files for SUMO Simulation */
 public class MapSumoConfig {
-    private MapSumoConfig(){ } // no default constructor .. not changeable
+  private MapSumoConfig() {} // no default constructor .. not changeable
 
-    public static final String SUMO_BIN = "sumo";
-    // The SUMO binary (can be "sumo" or "sumo-gui")
-    public static final String CONFIG_FILE = "src/main/resources/com/example/guifx/SumoConfig/complex.sumocfg";
-    //This file defines the network, routes, and simulation parameters.
-    public static final double STEP_LENGTH = 0.05;
-    //time step in seconds (0.1 means 10 steps per second)
-
-
-
+  public static final String SUMO_BIN = "sumo";
+  // The SUMO binary (can be "sumo" or "sumo-gui")
+  public static final String CONFIG_FILE =
+      "src/main/resources/com/example/guifx/SumoConfig/complex.sumocfg";
+  // This file defines the network, routes, and simulation parameters.
+  public static final double STEP_LENGTH = 0.05;
+  // time step in seconds (0.1 means 10 steps per second)
 
 }
